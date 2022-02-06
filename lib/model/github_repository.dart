3cf2d9afc62c34a374a5fdbe34a2bb7a2c6d5d79
morikann <mrknt0088@gmail.com>
2022-02-06 14:13:00 +0900

    dataからGithubRepositoryにマッピング

--- lib/model/github_repository.dart
+++ lib/model/github_repository.dart
@@ -9,6 +9,19 @@ class GithubRepository {
     this.issueCount,
   });
 
+  factory GithubRepository.from(Map<String, dynamic> data) {
+    return GithubRepository(
+      name: data['full_name'] as String,
+      ownerAvatarUrl:
+          (data['owner'] as Map<String, dynamic>)['avatar_url'] as String,
+      language: (data['language'] ?? '') as String,
+      starCount: data['stargazers_count'] as int,
+      watcherCount: data['watchers_count'] as int,
+      folkCount: data['forks_count'] as int,
+      issueCount: data['open_issues_count'] as int,
+    );
+  }
+
   final String? name;
   final String? ownerAvatarUrl;
   final String? language;
